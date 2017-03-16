c0686355c86d3b86de14fdf577fc1f178397cff4
lcjingdi <lcjingdi@gmail.com>
2017-03-16 18:25:23 +0800

    测试iOS7/8代码

--- WebViewDemo/WebViewDemo/ViewController.m
+++ WebViewDemo/WebViewDemo/ViewController.m
@@ -7,23 +7,64 @@
 //
 
 #import "ViewController.h"
+#import "WebViewJavascriptBridge.h"
+
 
 @interface ViewController ()
+@property (nonatomic, strong) UIWebView *webView1;
+//@property (nonatomic, strong) WKWebView *webView2;
 
+@property WebViewJavascriptBridge *bridge1;
+//@property WebViewJavascriptBridge *bridge2;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    [self uiwebview];
+//    [self wkwebView];
 }
 
+- (void)uiwebview {
+    [self.view addSubview:self.webView1];
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"Test" ofType:@"html"];
+    NSString *html = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+    [self.webView1 loadHTMLString:html baseURL:nil];
+    _bridge1 = [WebViewJavascriptBridge bridgeForWebView:self.webView1];
+    [_bridge1 setWebViewDelegate:self];
+    
+    [self.bridge1 registerHandler:@"Greet" handler:^(id data, WVJBResponseCallback responseCallback) {
+        NSLog(@"监听到了js greet回调 %@ uiwebview", data);
+    }];
+}
+//- (void)wkwebView {
+//    [self.view addSubview:self.webView2];
+//    NSString *path = [[NSBundle mainBundle] pathForResource:@"Test2" ofType:@"html"];
+//    NSString *html = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+//    [self.webView2 loadHTMLString:html baseURL:nil];
+//    _bridge2 = [WebViewJavascriptBridge bridgeForWebView:self.webView2];
+//    [_bridge2 setWebViewDelegate:self];
+//    
+//    [self.bridge2 registerHandler:@"Greet" handler:^(id data, WVJBResponseCallback responseCallback) {
+//        NSLog(@"监听到了js greet回调 %@ wkwebView", data);
+//    }];
+//}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (UIWebView *)webView1 {
+    if (_webView1 == nil) {
+        _webView1 = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height/2.0)];
+    }
+    return _webView1;
 }
 
-
+//- (WKWebView *)webView2 {
+//    if (_webView2 == nil) {
+//        
+//        _webView2 = [[WKWebView alloc] initWithFrame:CGRectMake(0, [UIScreen mainScreen].bounds.size.height/2.0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height/2.0)];
+//    }
+//    return _webView2;
+//}
 @end
